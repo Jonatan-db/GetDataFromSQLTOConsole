f17c1c1edee271e9f4a732645187f5fe039763e6
Jonatandb <jonatan.bonet@hasar.com>
2020-05-11 17:14:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+using System.Collections.Generic;
+using ConsoleTables;
+
+namespace RetrieveDataFromSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connecting to SQL Server!");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Cadena de conexión detectada en el archivo app.config:");
+            Console.WriteLine("------------------------------------------------------");
+            ShowConnectionStrings();
+            Console.WriteLine();
+
+            Console.WriteLine("Datos de la tabla Clientes:");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine();
+
+            ShowData("SELECT * FROM Clientes");
+            Console.ReadKey();
+        }
+
+        static void ShowConnectionStrings()
+        {
+            var settings = ConfigurationManager.ConnectionStrings;
+
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    Console.WriteLine("Name:              " + cs.Name);
+                    Console.WriteLine("Provider:          " + cs.ProviderName);
+                    Console.WriteLine("Connection String: " + cs.ConnectionString);
+                }
+            }
+        }
+
+        private static void ShowData(string query)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["SQL_Testing_Server"].ConnectionString;
+            DataSet datosEnMemoria = new DataSet();
+            datosEnMemoria = SelectRows(datosEnMemoria, connectionString, query);
+
+            // Usando ConsoleTables:
+            ConsoleTable table = new ConsoleTable(datosEnMemoria.Tables[0].Columns.ToString());
+            for (int i = 0; i< datosEnMemoria.Tables[0].Rows.Count; i++)
+            {
+                table.Rows.Add(datosEnMemoria.Tables[0].Rows[i].ItemArray);
+            }
+            table.Write();
+
+            // Usando ExtensionMethods:
+            Console.WriteLine(datosEnMemoria.ToPrettyString());
+        }
+
+        private static DataSet SelectRows(DataSet dataset, string connectionString, string queryString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = new SqlCommand(queryString, connection);
+                adapter.Fill(dataset);
+                return dataset;
+            }
+        }
+
+    } 
+    static class ExtensionMethods
+        {
+            public static string ToPrettyString(this DataSet ds)
+            {
+                var sb = new StringBuilder();
+                foreach (var table in ds.Tables.ToList())
+                {
+                    sb.AppendLine("--" + table.TableName + "--");
+                    sb.AppendLine(String.Join(" | ", table.Columns.ToList()));
+                    foreach (DataRow row in table.Rows)
+                    {
+                        sb.AppendLine(String.Join(" | ", row.ItemArray));
+                    }
+                    sb.AppendLine();
+                }
+                return sb.ToString();
+            }
+
+            public static void AddRange(this DataColumnCollection collection, params string[] columns)
+            {
+                foreach (var column in columns)
+                {
+                    collection.Add(column);
+                }
+            }
+
+            public static List<DataTable> ToList(this DataTableCollection collection)
+            {
+                var list = new List<DataTable>();
+                foreach (var table in collection)
+                {
+                    list.Add((DataTable)table);
+                }
+                return list;
+            }
+
+            public static List<DataColumn> ToList(this DataColumnCollection collection)
+            {
+                var list = new List<DataColumn>();
+                foreach (var column in collection)
+                {
+                    list.Add((DataColumn)column);
+                }
+                return list;
+            }
+        }
+
+}
---
+++ Readme.md
@@ -0,0 +1,86 @@
+﻿
+Jonatandb - 11-05-2020
+----------------------
+
+Uso de una app de consola para conectarse a un SQL Server y mostrar los datos de alguna tabla.
+Es obligatorio mejorarlo :-)
+
+Para ejecutar:
+--------------
+ - Desde la consola, ir a la carpeta del proyecto, luego ejecutar:
+   dotnet build GetDataFromSQL.csproj
+   dotnet run
+
+ - Desde Visual Studio:
+   Presionar F5
+ 
+Instalé los siguientes paquetes de Nuget:
+-----------------------------------------
+
+ - System.Configuration.ConfigurationManager 4.7.0
+   Permite acceder a las cadenas de conexion en el archivo de configuración de la aplicación: app.config.
+ 
+ - ConsoleTables 2.4.1
+   Permite mostrar de una forma "linda" los datos de una tabla.
+
+ - También agregué la clase "ExtensionMethods", que provee otra forma de mostrar el contenido de las tablas.
+
+Sitios investigados:
+--------------------
+
+  - .NET Core documentation
+    - https://docs.microsoft.com/en-us/dotnet/core/
+  
+  - Manage dependencies in .NET Core applications
+    - https://docs.microsoft.com/en-us/dotnet/core/tools/dependencies
+  
+  - Create a .NET Core application with plugins
+    - https://docs.microsoft.com/en-us/dotnet/core/tutorials/creating-app-with-plugin-support
+  
+  - Inicio rápido: Instalación y uso de un paquete en Visual Studio (solo Windows)
+    - https://docs.microsoft.com/es-es/nuget/quickstart/install-and-use-a-package-in-visual-studio
+  
+  - System.Data.SqlClient Namespace
+    - https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient?view=dotnet-plat-ext-3.1
+  
+  - SqlDataAdapter Class
+    - https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldataadapter?view=dotnet-plat-ext-3.1
+  
+  - Retrieving and Modifying Data in ADO.NET
+    - https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/retrieving-and-modifying-data?view=dotnet-plat-ext-3.1
+  
+  - Connecting to a Data Source in ADO.NET
+    - https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/connecting-to-a-data-source
+  
+  - Establishing the Connection
+    - https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/establishing-the-connection
+
+  - Connection Strings in ADO.NET
+    - https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/connection-strings
+
+  - Connection String Builders
+    - https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/connection-string-builders
+  
+  - Connection Strings and Configuration Files
+    - https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/connection-strings-and-configuration-files
+  
+  - Is ConfigurationManager.AppSettings available in .NET Core 2.0?
+    - https://stackoverflow.com/questions/47591910/is-configurationmanager-appsettings-available-in-net-core-2-0
+  
+  - How to store and retrieve custom information from an application configuration file by using Visual C#
+    - https://support.microsoft.com/en-us/help/815786/how-to-store-and-retrieve-custom-information-from-an-application-confi
+  
+  - How to get the connection String from a database
+    - https://stackoverflow.com/questions/10479763/how-to-get-the-connection-string-from-a-database
+  
+  - using DataSet in Console Application?
+    - https://stackoverflow.com/questions/33662631/using-dataset-in-console-application
+  
+  - Print DataTable to Console (and more)
+    - https://www.codeproject.com/Tips/1147879/Print-DataTable-to-Console-and-more
+
+  - ConsoleTables 2.4.1
+    - https://www.nuget.org/packages/ConsoleTables/
+  
+  - khalidabuhakmeh / ConsoleTables
+    - https://github.com/khalidabuhakmeh/ConsoleTables
